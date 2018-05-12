63a453935f322e92426f9ef7783c0ce38dfadb9f
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-05-12 20:13:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@ fizz_3 = int % 3
 fizz_5 = int % 5
 fizz_15 = (fizz_3 || fizz_5) == 0
 fizz_4 = nil
-  if fizz_3 == 0
-    "Fizz"
+  if fizz_15 == 0
+    "FizzBuzz"
   elsif fizz_5 == 0
     "Buzz"
-  elsif fizz_15 == 0
-    "FizzBuzz"
+  elsif fizz_3 == 0
+    "Fizz"
   else
     fizz_4
   end
