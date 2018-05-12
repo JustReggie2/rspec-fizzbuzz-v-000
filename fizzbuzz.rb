cd11b2a1e17a94aae5ed3abb7f9b4615bc458dd8
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-05-12 18:28:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+fizz_3 = 3 == 0
+fizz_5 = 5 == 0
+fizz_15 = 3 || 5 == 0
+fizz_4 = nil
+  if int % fizz_3
     "Fizz"
-  elsif int % 5 == 0
+  elsif int % fizz_5
     "Buzz"
-  elsif int % 3 || 5 == 0
+  elsif int % fizz_15
     "FizzBuzz"
   else
     nil
